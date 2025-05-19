8366cba116acc5287d61f29e307b87972147cee4
restinbeat <thek226@gmail.com>
2025-05-19 10:27:32 +0900

    feat: multiple coupons request

--- api/src/test/java/com/example/api/service/ApplyServiceTest.java
+++ api/src/test/java/com/example/api/service/ApplyServiceTest.java
@@ -28,4 +28,28 @@ class ApplyServiceTest {
 
         assertThat(count).isEqualTo(1);
     }
+
+    @Test
+    public void 여러명응모() throws InterruptedException {
+        int threadCount = 1000;
+        CountDownLatch latch = new CountDownLatch(threadCount);
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+
+        for (int i = 0; i < threadCount; i++) {
+            long userId = i;
+            executorService.submit(() -> {
+                try {
+                    applyService.apply(userId);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        long count = couponRepository.count();
+
+        assertThat(count).isEqualTo(100);
+    }
 }
