e70ca75a52e94daeba8e3ca243d77fa1ad66dc1f
restinbeat <thek226@gmail.com>
2025-06-05 13:23:00 +0900

    chore: setting rollback

--- api/src/test/java/com/example/api/service/ApplyServiceTest.java
+++ api/src/test/java/com/example/api/service/ApplyServiceTest.java
@@ -1,16 +1,15 @@
 package com.example.api.service;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import com.example.api.repository.CouponRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import com.example.api.repository.CouponRepository;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class ApplyServiceTest {
@@ -21,20 +20,20 @@ class ApplyServiceTest {
     @Autowired
     private CouponRepository couponRepository;
 
-    // @Test
-    // public void 한번만응모() {
-    //     applyService.apply(1L);
+    @Test
+    public void 한번만응모() {
+        applyService.apply(1L);
 
-    //     long count = couponRepository.count();
+        long count = couponRepository.count();
 
-    //     assertThat(count).isEqualTo(1);
-    // }
+        assertThat(count).isEqualTo(1);
+    }
 
     @Test
     public void 여러명응모() throws InterruptedException {
         int threadCount = 1000;
-        CountDownLatch latch = new CountDownLatch(threadCount);
         ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
 
         for (int i = 0; i < threadCount; i++) {
             long userId = i;
@@ -48,6 +47,8 @@ class ApplyServiceTest {
         }
 
         latch.await();
+
+        Thread.sleep(10000);
 
         long count = couponRepository.count();
 
