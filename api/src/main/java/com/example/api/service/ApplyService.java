cc9392b83761be522096d28b5c50826bf91234c5
restinbeat <thek226@gmail.com>
2025-06-09 10:35:29 +0900

    feat: 유저당 1개 쿠폰 제한

--- api/src/main/java/com/example/api/service/ApplyService.java
+++ api/src/main/java/com/example/api/service/ApplyService.java
@@ -1,6 +1,7 @@
 package com.example.api.service;
 
 import com.example.api.domain.Coupon;
+import com.example.api.repository.AppliedUserRepository;
 import com.example.api.repository.CouponCountRepository;
 import com.example.api.repository.CouponRepository;
 import com.example.api.producer.CouponCreateProducer;
@@ -15,16 +16,28 @@ public class ApplyService {
 
     private final CouponCreateProducer couponCreateProducer;
 
+    private final AppliedUserRepository appliedUserRepository;
+
     public ApplyService(CouponRepository couponRepository,
         CouponCountRepository couponCountRepository,
-        CouponCreateProducer couponCreateProducer) {
+        CouponCreateProducer couponCreateProducer,
+        AppliedUserRepository appliedUserRepository
+    ) {
         this.couponRepository = couponRepository;
         this.couponCountRepository = couponCountRepository;
         this.couponCreateProducer = couponCreateProducer;
+        this.appliedUserRepository = appliedUserRepository;
     }
 
     public void apply(Long userId) {
+
+        Long apply = appliedUserRepository.add(userId);
 //        long count = couponRepository.count();
+
+        if (apply != 1) {
+            return;
+        }
+
         Long count = couponCountRepository.increment();
 
         if (count > 100) {
